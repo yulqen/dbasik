438bbb1824d566227c5e1237cda73a6af945c403
Matthew Lemon <lemon@matthewlemon.com>
2018-04-15 16:32:57 +0100

    initial commit - including index page and environment setup

---
+++ config/settings/local.py
@@ -0,0 +1,22 @@
+import os
+
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+INSTALLED_APPS.append('django_extensions')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'localhost',
+        'NAME': 'dbasik',
+        'USER': 'lemon',
+        'PORT': '5432',
+        'PASSWORD': 'lemon'
+    }
+}
