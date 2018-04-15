07326d7dce564ee36df63007407513fefaa7c004
Matthew Lemon <lemon@matthewlemon.com>
2018-04-15 18:09:51 +0100

    allowed immediate authentication and login of new registering user

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 def index(request):
@@ -7,6 +8,18 @@ def index(request):
 
 
 def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+
     form = UserCreationForm()
     context = {'form': form}
     return render(request, 'registration/register.html', context)
