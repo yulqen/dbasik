ba4582fa1ce678a2a029f31a0e79b9c560d3bdf8
Matthew Lemon <lemon@matthewlemon.com>
2018-04-25 20:58:22 +0100

    started working on the datamap page

--- datamap/views.py
+++ datamap/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views.generic import ListView
-from .models import Datamap
+from .models import Datamap, DatamapLine
 from .forms import CreateDatamapForm
 
 
 class DatamapList(ListView):
     model = Datamap
+
+
+def datamap_view(request, dm_pk):
+    dm_lines = DatamapLine.objects.filter(datamap_id=dm_pk)
+    context = {'dm_lines': dm_lines}
+    return render(request, 'datamap/datamap.html', context)
 
 
 def create_datamap(request):
