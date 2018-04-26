2266c6f405bc82eae184339074af77b18d0fd3ca
Matthew Lemon <lemon@matthewlemon.com>
2018-04-26 05:54:02 +0100

    swapped to using a class based datamap detail view as experiment

--- datamap/urls.py
+++ datamap/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import DatamapList, create_datamap, datamap_view
+from . views import DatamapList, create_datamap, DatamapDetail
 
 urlpatterns = [
     path('datamaps/', DatamapList.as_view(), name='datamaps'),
     path('createdatamap/', create_datamap, name='createdatamap'),
-    path('datamap/<int:dm_pk>', datamap_view, name='datamap'),
+#    path('datamap/<int:dm_pk>', datamap_view, name='datamap'),
+    path('datamap/<int:pk>', DatamapDetail.as_view(), name='datamap'),
 ]
--- datamap/views.py
+++ datamap/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Datamap, DatamapLine
 from .forms import CreateDatamapForm
 
@@ -9,10 +9,22 @@ class DatamapList(ListView):
     model = Datamap
 
 
-def datamap_view(request, dm_pk):
-    dm_lines = DatamapLine.objects.filter(datamap_id=dm_pk)
-    context = {'dm_lines': dm_lines}
-    return render(request, 'datamap/datamap.html', context)
+class DatamapDetail(DetailView):
+    model = DatamapLine
+    template_name = 'datamap/datamap_detail.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        dm_lines = DatamapLine.objects.filter(datamap_id=2)
+        context['dm_lines'] = dm_lines
+        return context
+
+
+# def datamap_view(request, dm_pk):
+#     dm_lines = DatamapLine.objects.filter(datamap_id=dm_pk)
+#     context = {'dm_lines': dm_lines}
+#     return render(request, 'datamap/datamap.html', context)
 
 
 def create_datamap(request):
