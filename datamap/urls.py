e27a85596b638fd67d6bac3d282d18ae98c1e77d
Matthew Lemon <lemon@matthewlemon.com>
2018-04-26 06:17:50 +0100

    gone back to function based views for datamap

--- datamap/urls.py
+++ datamap/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from . views import DatamapList, create_datamap, DatamapDetail
+from . views import DatamapList, create_datamap, datamap_view
 
 urlpatterns = [
     path('datamaps/', DatamapList.as_view(), name='datamaps'),
     path('createdatamap/', create_datamap, name='createdatamap'),
-#    path('datamap/<int:dm_pk>', datamap_view, name='datamap'),
-    path('datamap/<int:pk>', DatamapDetail.as_view(), name='datamap'),
+    path('datamap/<int:dm_pk>', datamap_view, name='datamap'),
 ]
--- datamap/views.py
+++ datamap/views.py
@@ -9,22 +9,10 @@ class DatamapList(ListView):
     model = Datamap
 
 
-class DatamapDetail(DetailView):
-    model = DatamapLine
-    template_name = 'datamap/datamap_detail.html'
-
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        dm_lines = DatamapLine.objects.filter(datamap_id=2).order_by('id')
-        context['dm_lines'] = dm_lines
-        return context
-
-
-# def datamap_view(request, dm_pk):
-#     dm_lines = DatamapLine.objects.filter(datamap_id=dm_pk)
-#     context = {'dm_lines': dm_lines}
-#     return render(request, 'datamap/datamap.html', context)
+def datamap_view(request, dm_pk):
+    dm_lines = DatamapLine.objects.filter(datamap_id=dm_pk).order_by('id')
+    context = {'dm_lines': dm_lines, 'dm_pk': dm_pk}
+    return render(request, 'datamap/datamap.html', context)
 
 
 def create_datamap(request):
