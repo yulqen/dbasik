84b21997dee3b1fba2deee27c9d825608e5a632d
Matthew Lemon <lemon@matthewlemon.com>
2018-05-02 08:48:44 +0100

    small addition to tests for csv reading

--- datamap/tests/conftest.py
+++ datamap/tests/conftest.py
@@ -1,5 +1,8 @@
-import pytest
+import csv
 import tempfile
+
+import pytest
+
 
 TEMPDIR = tempfile.gettempdir()
 
@@ -8,6 +11,10 @@ TEMPDIR = tempfile.gettempdir()
 def uploaded_csv_file():
     uf = "/".join([TEMPDIR, 'datamap.csv'])
     with open(uf, 'w') as f:
-        f.write("First line")
-        f.write("Second line")
+        fieldnames = ['header_1', 'header_2', 'header_3']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow({'header_1': "First row col 1", 'header_2': 'First row col 2', 'header_3': 'First row col 3'})
+        writer.writerow({'header_1': "Second row col 1", 'header_2': 'Second row col 2', 'header_3': 'Second row col 3'})
+        writer.writerow({'header_1': "Third row col 1", 'header_2': 'Third row col 2', 'header_3': 'Third row col 3'})
     return uf
--- datamap/tests/test_file_uploads.py
+++ datamap/tests/test_file_uploads.py
@@ -1,3 +1,5 @@
+import csv
+
 from django.test import TestCase
 from django.core.files.uploadedfile import UploadedFile
 
@@ -6,3 +8,17 @@ def test_uploaded_file(uploaded_csv_file):
     test_file = open(uploaded_csv_file)
     uf = UploadedFile(test_file)
     assert uf.name == 'datamap.csv'
+
+
+def test_csv_contents(uploaded_csv_file):
+    test_file = open(uploaded_csv_file)
+    uf = UploadedFile(test_file)
+    csv_reader = csv.DictReader(uf)
+    line = next(csv_reader)
+    assert line['header_1'] == 'First row col 1'
+    assert line['header_2'] == 'First row col 2'
+    assert line['header_3'] == 'First row col 3'
+    line = next(csv_reader)
+    assert line['header_1'] == 'Second row col 1'
+    assert line['header_2'] == 'Second row col 2'
+    assert line['header_3'] == 'Second row col 3'
