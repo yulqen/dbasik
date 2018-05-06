628c7739c5ce71c7fcce1a3baceb6a9e9051a561
Matthew Lemon <lemon@matthewlemon.com>
2018-05-06 17:26:13 +0100

    fixed test

--- datamap/tests/test_file_processors.py
+++ datamap/tests/test_file_processors.py
@@ -1,6 +1,6 @@
-from helpers.file_processors import add_datamaplines_from_csv
+from helpers.file_processors import _validate_dmlines_from_csv
 
 
 def test_csv_processor(uploaded_csv_file_bytes):
 
-    assert add_datamaplines_from_csv(uploaded_csv_file_bytes) == (4, [])
+    assert _validate_dmlines_from_csv(uploaded_csv_file_bytes) == (4, [])
