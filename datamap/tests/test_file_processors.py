1e7dcb6e15cf37f5eec3a3a35c38b058e7440c01
Matthew Lemon <lemon@matthewlemon.com>
2018-05-07 07:01:21 +0100

    big refactor - removed file_processors.py and cleaned up

--- datamap/tests/test_file_processors.py
+++ datamap/tests/test_file_processors.py
@@ -1,6 +1,1 @@
-from helpers.file_processors import _validate_dmlines_from_csv
-
-
-def test_csv_processor(uploaded_csv_file_bytes):
-
-    assert _validate_dmlines_from_csv(uploaded_csv_file_bytes) == (4, [])
+# to do
--- helpers/file_handling.py
+++ helpers/file_handling.py
@@ -1,7 +1,22 @@
-from helpers.file_processors import _validate_dmlines_from_csv
+import codecs
+import csv
+import inspect
+import re
+from importlib import import_module
+from typing import Tuple, List
 
+from django import forms
 from django.core.files.uploadedfile import UploadedFile
 
+from datamap.models import DatamapLine
+from exceptions import IncorrectHeaders
+
+
+# used in introspecting the model for datamap class
+# TODO this should come from the config and might not
+# even be needed at all. Check the CSVFileUpload class
+
+target_model_exclude = "datamap"
 
 acceptable_types = {
     "csv": ["text/csv"],
@@ -11,6 +26,16 @@ acceptable_types = {
         "application/vnd.ms-excel.sheet.macroenabled.12",
     ],
 }
+
+
+class CSVForm(forms.ModelForm):
+    """
+    Used to verify an uploaded CSV file, line-by-line.
+    """
+
+    class Meta:
+        model = DatamapLine
+        exclude = [target_model_exclude]  # this is the ForeignKey
 
 
 class DBUploadedFile:
@@ -32,8 +57,69 @@ class CSVUploadedFile(DBUploadedFile):
     """Handles uploaded CSV files.
     """
 
+    def _validate_dmlines_from_csv(
+        self, csv_file: UploadedFile
+    ) -> Tuple[int, List[dict]]:
+        """
+        Use a Form to validate input in an uploaded CSV
+        file.
+        :param csv_file: an opened file object
+        :return: tuple of records added and errors
+        """
+
+        _id_regex = re.compile(".*_id$", re.I)
+
+        records_added = 0
+        errors = []
+
+        csv_reader = csv.DictReader(codecs.iterdecode(csv_file, "utf-8"))
+        for row in csv_reader:
+            form = CSVForm(row)
+            if form.is_valid():
+                # process
+                records_added += 1
+            else:
+                try:
+                    for k in form.errors.keys():
+                        print(
+                            f"Error in {k}: {form.data[k]}\n"
+                            f"Detail: {form.errors[k][0]}"
+                        )
+                    errors.append(form.errors)
+                except KeyError:
+                    # introspect models.py to get correct expected csv headers
+                    # and send with this exception
+
+                    _models = import_module(f"{target_model_exclude}.models")
+                    _model_classes = [
+                        (k, v)
+                        for k, v in inspect.getmembers(_models)
+                        if inspect.isclass(v)
+                    ]
+                    _keys = [
+                        name
+                        for name in _model_classes[1][1].__dict__.keys()
+                        if name not in [
+                            "id",
+                            "__module__",
+                            "__doc__",
+                            "_meta",
+                            "DoesNotExist",
+                            "MultipleObjectsReturned",
+                            "objects",
+                            "__str__",
+                        ]
+                    ]
+                    _keys = [k for k in _keys if not re.match(_id_regex, k)]
+                    _keys = [k for k in _keys if k != target_model_exclude]
+
+                    raise IncorrectHeaders(
+                        f"Incorrect headers in csv file - should be: {','.join(_keys)}"
+                    )
+        return records_added, errors
+
     def process(self):
-        res = _validate_dmlines_from_csv(self._uploaded_file)
+        res = self._validate_dmlines_from_csv(self._uploaded_file)
         print(res)
 
 
