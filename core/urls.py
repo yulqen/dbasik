43f2322de2eacb72608a5db40a1cad196c7004a0
Matthew Lemon <lemon@matthewlemon.com>
2018-05-19 05:28:22 +0100

    added login then logout view

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth.views import logout_then_login
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('register', views.register, name='register'),
+    path('logout-then-login', logout_then_login, name='logout-the-login')
 ]
