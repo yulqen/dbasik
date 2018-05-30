a07e5658b280de51ca9908c7e8e3c6c7a758db2f
Matthew Lemon <lemon@matthewlemon.com>
2018-05-30 11:19:51 +0100

    added a cancel button to the Project Type create form

--- register/forms.py
+++ register/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from django.urls import reverse
 from .import models
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, ButtonHolder, Fieldset, Button
 
 
 class ProjectTypeCreateForm(forms.ModelForm):
@@ -13,7 +14,19 @@ class ProjectTypeCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        cancel_redirect = reverse('register:projecttype_list')
+
         self.helper = FormHelper(self)
         self.helper.form_class = "form-group"
         self.helper.form_method = "post"
-        self.helper.layout.append(Submit('save', 'Save'))
+        self.helper.layout = Layout(
+            Fieldset(
+                'Create a new Project Type',
+                'name',
+                'description',
+            ),
+            ButtonHolder(
+                Submit('submit', 'Submit'),
+                Button('cancel', 'Cancel', onclick=f"location.href='{cancel_redirect}';", css_class="btn btn-danger")
+            )
+        )
