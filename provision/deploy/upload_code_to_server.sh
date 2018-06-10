412aed22b905bd657ec853804f93e758dfd57ff0
Matthew Lemon <lemon@matthewlemon.com>
2018-06-10 21:08:24 +0100

    added a line to provision script to start and stop server

--- provision/deploy/upload_code_to_server.sh
+++ provision/deploy/upload_code_to_server.sh
@@ -2,3 +2,4 @@
 
 echo "Uploading code to 209.97.142.1\n"
 rsync -avzh --exclude={'driver/','.mypy_cache/','.pytest_cache/','.ropeproject/','.vagrant/','.idea','.git','.pytest_cache/*'} --delete ~/code/python/dbasik-dev/dbasik_dftgovernance/ dbasik@209.97.142.1:/dbasik/code/dbasik_dftgovernance/
+ssh dbasik@dbasik "pkill gunicorn & bash /dbasik/code/dbasik_dftgovernance/gunicorn_start.bash"
