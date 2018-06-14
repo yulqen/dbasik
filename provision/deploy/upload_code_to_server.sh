475ea9de62db7c74de8fa8f244d437cf5b4fe8a8
Matthew Lemon <lemon@matthewlemon.com>
2018-06-14 20:31:41 +0100

    removed extraneous ansible files

--- provision/deploy/upload_code_to_server.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-
-echo "Uploading code to 209.97.142.1\n"
-rsync -avzh --exclude={'driver/','logs/','.mypy_cache/','.pytest_cache/','.ropeproject/','.vagrant/','.idea','.git','.pytest_cache/*'} --delete ~/code/python/dbasik-dev/dbasik_dftgovernance/ dbasik@209.97.142.1:/dbasik/code/dbasik_dftgovernance/
-# you have to start and stop the server manually. Not doing it causes issues with terminal hooking, etc
-#ssh dbasik@dbasik "pkill gunicorn & bash /dbasik/code/dbasik_dftgovernance/gunicorn_start.bash"
