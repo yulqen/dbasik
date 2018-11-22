aeedf7b248122e7559996c2ccee012803bb9d24e
Matthew Lemon <lemon@matthewlemon.com>
2018-11-22 07:11:18 +0000

    fixed shebang in upload script

--- provision/upload_code_to_server.sh
+++ provision/upload_code_to_server.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 echo "Uploading code to 209.97.142.1\n"
 rsync -avzh --exclude={'__pycache__/','tests/','driver/','logs/','.mypy_cache/','.pytest_cache/','.ropeproject/','.vagrant/','.idea','.git','.pytest_cache/*'} --delete ~/code/python/dbasik-dev/dbasik-dftgovernance/ dbasik@209.97.142.1:/dbasik/code/dbasik_dftgovernance/
