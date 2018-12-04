ea61741018357abbf60e6d1928d8606f537239b7
Matthew Lemon <lemon@matthewlemon.com>
2018-12-04 16:50:12 +0000

    can now parse a single spreadsheet by uploading a file

--- excelparser/views.py
+++ excelparser/views.py
@@ -1,8 +1,16 @@
+import os
+
+from pudb import set_trace
+
 from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.core.files.storage import default_storage
+from django.core.files.uploadedfile import UploadedFile
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from excelparser.forms import ProcessPopulatedTemplateForm
+from excelparser.helpers.parser import ParsedSpreadsheet
 
 
 class ProcessPopulatedTemplate(FormView):
@@ -17,7 +25,12 @@ class ProcessPopulatedTemplate(FormView):
         return str(reverse_lazy("returns:return_data", args=[self.kwargs['return_id']]))
 
     def form_valid(self, form):
-        print("Here is where we process the spreadsheet!")
-        # this is a form to handle a single form - we will do zip files elsewhere
-        print(self.request.FILES['source_file'])
+        uploaded_file: UploadedFile = self.request.FILES['source_file']
+        save_path = os.path.join(settings.MEDIA_ROOT, 'uploads', uploaded_file.name)
+        path = default_storage.save(save_path, uploaded_file)
+        project = form.cleaned_data['return_obj'].project
+        return_obj = form.cleaned_data['return_obj']
+        datamap = form.cleaned_data['datamap']
+        parsed_spreadsheet = ParsedSpreadsheet(path, project, return_obj, datamap)
+        parsed_spreadsheet.process()
         return HttpResponseRedirect(self.get_success_url())
