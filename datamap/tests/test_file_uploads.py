27e6372c296219c03a658b74f5bdb559a5f31750
Matthew Lemon <lemon@matthewlemon.com>
2018-12-16 07:42:43 +0000

    removed old pytest tests

--- datamap/tests/test_file_uploads.py
+++ datamap/tests/test_file_uploads.py
@@ -1,8 +1,6 @@
-import csv
 import os
 import tempfile
 
-from django.core.files.uploadedfile import UploadedFile
 from django.test import TestCase
 
 from datamap.helpers import check_duplicate_lines
@@ -46,30 +44,3 @@ class TestDuplicateMultiCSVLines(TestCase):
     def test_duplicate_lines(self):
         dup_str = check_duplicate_lines(self.df)
         self.assertEqual(dup_str, self.expected_str)
-
-
-def test_good_csv_file(good_csv_file):
-    with open(good_csv_file, "r") as f:
-        uf = UploadedFile(f)
-        csv_reader = csv.DictReader(uf)
-        line = next(csv_reader)
-        assert line["key"] == "First row col 1"
-        assert line["sheet"] == "First row col 2"
-        assert line["cell_ref"] == "A15"
-        line = next(csv_reader)
-        assert line["key"] == "Second row col 1"
-        assert line["sheet"] == "Second row col 2"
-        assert line["cell_ref"] == "B15"
-
-
-def test_csv_contents(uploaded_csv_file):
-    uf = UploadedFile(uploaded_csv_file)
-    csv_reader = csv.DictReader(uf)
-    line = next(csv_reader)
-    assert line["key"] == "First row col 1UL"
-    assert line["sheet"] == "First row col 2UL"
-    assert line["cell_ref"] == "A15"
-    line = next(csv_reader)
-    assert line["key"] == "Second row col 1UL"
-    assert line["sheet"] == "Second row col 2UL"
-    assert line["cell_ref"] == "B15"
