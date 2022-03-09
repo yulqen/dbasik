212034dcec5e670eea71fd652dcc63e1a566ba82
Matthew Lemon <chaffinach+git@protonmail.ch>
2022-03-09 06:24:22 +0000

    wip: fixing core links

--- dbasik/core/urls.py
+++ dbasik/core/urls.py
@@ -3,7 +3,8 @@ from django.contrib.auth.views import logout_then_login
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('logout-then-login/', logout_then_login, name='logout-the-login'),
     path('profile/', views.profile, name='profile')
 ]
--- dbasik/core/views.py
+++ dbasik/core/views.py
@@ -1,9 +1,5 @@
-from django.contrib import messages
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
+from django.shortcuts import render
 
 
 @login_required
