a2e15ab139a7b4fae27f4277fd7c008a7fb91fb8
Matthew Lemon <matt@matthewlemon.com>
2022-10-11 17:28:50 +0100

    datamap tests now uses nested output

--- dbasik/datamap/api.py
+++ dbasik/datamap/api.py
@@ -1,22 +1,40 @@
 from typing import List
 
-from ninja import ModelSchema, Router
+from dbasik.register.api import TierSchema
+from ninja import ModelSchema, Router, Schema
 
 from .models import Datamap, DatamapLine
 
 router = Router()
 
 
-class DatamapLineSchema(ModelSchema):
-    class Config:
-        model = DatamapLine
-        model_fields = ["id", "datamap", "key", "data_type", "sheet", "cell_ref"]
+# class DatamapSchema(ModelSchema):
+#     class Config:
+#         model = Datamap
+#         model_fields = ["id", "name", "tier", "active", "slug"]
 
 
-class DatamapSchema(ModelSchema):
-    class Config:
-        model = Datamap
-        model_fields = ["id", "name", "tier", "active", "slug"]
+class DatamapSchema(Schema):
+    id: int
+    name: str
+    tier: TierSchema = None  # optional, as per ninja docs
+    active: bool
+    slug: str
+
+
+class DatamapLineSchema(Schema):
+    id: int
+    datamap: DatamapSchema = None
+    key: str
+    data_type: str
+    sheet: str
+    cell_ref: str
+
+
+# class DatamapLineSchema(ModelSchema):
+#     class Config:
+#         model = DatamapLine
+#         model_fields = ["id", "datamap", "key", "data_type", "sheet", "cell_ref"]
 
 
 @router.get("/datamap", response=List[DatamapLineSchema])
